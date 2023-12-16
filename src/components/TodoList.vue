30247817bdc2049255ff772083e2e6bb590d09ba
Faruk Cagan <faruk.cagan@bestera.com.tr>
2023-12-16 23:39:52 +0300

    todo list added

---
+++ src/components/TodoList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Todo List</h1>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">{{ todo.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  mounted() {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        this.todos = data;
+      })
+      .catch((error) => {
+        console.error("veri çekme hatası:", error);
+      });
+  },
+};
+</script>
+
+<style scoped>
+div{
+    width:50%;
+}
+li {
+  background-color: grey;
+  margin-top: 5px;
+  padding: 20px;
+}
+</style>
